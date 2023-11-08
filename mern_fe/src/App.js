da9123f00c8b793937421159b1d1135c687373b0
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-08 16:45:27 -0500

    setting up App.js and running into invalid hook call error from @material-ui imports

--- mern_fe/src/App.js
+++ mern_fe/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import memories from './Images/memories.png'
 
 const App = () => {
     return (
+        // <Container maxWidth="lg">
+        //     <AppBar position="static" color="inherit">
+        //         <Typography variant="h2" align="center">memories</Typography>
+        //         <img src={memories} alt="memories" height="60"/>
+        //     </AppBar>
+        // </Container>
         <div>
-            <h1>App</h1>
+            <img src={memories} alt="memories" height="60"/>
+            <Grow></Grow>
+            Hello
         </div>
     )
 }

0de94d4d2ffac64ea792b05d797481ea13a6e5bc
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-15 23:07:32 -0500

    setting up form with regular html in parallel to @material to first successfully post data then continue to troubleshoot @material

--- mern_fe/src/Components/Form/Form.js
+++ mern_fe/src/Components/Form/Form.js
@@ -5,9 +5,7 @@ import styled from 'styled-components'
 import useStyles from './styles';
 
 const Form = () => {
-    const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
-    })
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
     // const classes = useStyles();
 
     const handleSubmit = () => {
@@ -20,20 +18,49 @@ const Form = () => {
     //         <Form autoComplete="off" noValidate className="classes.form" onSubmit="handleSubmit">
     //         <Typography varient="h6">Creating a Memory</Typography>
     //         <TextField 
-    //             name="Creator" 
+    //             name="creator" 
     //             varient="outlined" 
     //             label="Creator" 
     //             value={postData.creator}
-    //             onChange={}/>
+    //             onChange={(e) => setPostData({ ...postData, creator: e.target.value})}
+    //         />
+    //         <TextField 
+    //             name="title" 
+    //             varient="outlined" 
+    //             label="Title" 
+    //             value={postData.creator}
+    //             onChange={(e) => setPostData({ ...postData, creator: e.target.value})}
+    //         />
+    //         <TextField 
+    //             name="message" 
+    //             varient="outlined" 
+    //             label="Message" 
+    //             value={postData.creator}
+    //             onChange={(e) => setPostData({ ...postData, creator: e.target.value})}
+    //         />
+    //         <TextField 
+    //             name="tags" 
+    //             varient="outlined" 
+    //             label="Tags" 
+    //             value={postData.creator}
+    //             onChange={(e) => setPostData({ ...postData, creator: e.target.value})}
+    //         />
     //         </Form>
     //     </Paper> 
     //     </>
         <>
         <div>
-            <div>
-             <h6>Creating a Memory</h6>  
-             <div></div>
-            </div>
+            <form>
+             <h6>Creating a Memory</h6>
+             <label for="creator">Creator</label>  
+             <input type="text" id="creator" name="creator" onChange={(e) => setPostData({ ...postData, creator: e.target.value})} />
+             <label for="title">Title</label>
+             <input type="text" id="title" name="title" onChange={(e) => setPostData({ ...postData, creator: e.target.value})} />
+             <label for="message">Message</label>
+             <input type="text" id="message" name="message" onChange={(e) => setPostData({ ...postData, creator: e.target.value})} />
+             <label for="tags">Tags</label>
+             <input type="text" id="tags" name="tags" onChange={(e) => setPostData({ ...postData, creator: e.target.value})} />
+            </form>
         </div>
         </>
     )
